f654f83b5ae0bd672080e4b87533c88aa559633b
Mannat Garg <mannatgarg2006@gmail.com>
2025-02-28 10:13:22 +0530

    Completed the assignment

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Mannat Garg</h1>
+      <h2>Contact me: <a href="mailto:mannat.garg.s84@kalvium.community">mannat.garg.s84@kalvium.community</a>
+      </h2>
+    </div>
+  )
+}
